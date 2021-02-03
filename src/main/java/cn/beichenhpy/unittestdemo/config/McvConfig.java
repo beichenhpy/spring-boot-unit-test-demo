36d99a14022c3f874058cf9c0c0163f13620bf35
beichenhpy <beichenhpy@outlook.com>
2021-02-03 12:14:01 +0800

    1、add cors

--- src/main/java/cn/beichenhpy/unittestdemo/config/McvConfig.java
+++ src/main/java/cn/beichenhpy/unittestdemo/config/McvConfig.java
@@ -1,8 +1,12 @@
 package cn.beichenhpy.unittestdemo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.charset.StandardCharsets;
@@ -22,5 +26,20 @@ public class McvConfig implements WebMvcConfigurer {
                 .filter(converter -> converter instanceof MappingJackson2HttpMessageConverter)
                 .findFirst()
                 .ifPresent(converter -> ((MappingJackson2HttpMessageConverter) converter).setDefaultCharset(StandardCharsets.UTF_8));
+    }
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        /* 是否允许请求带有验证信息 */
+        corsConfiguration.setAllowCredentials(true);
+        /* 允许访问的客户端域名 */
+        corsConfiguration.addAllowedOrigin("*");
+        /* 允许服务端访问的客户端请求头 */
+        corsConfiguration.addAllowedHeader("*");
+        /* 允许访问的方法名,GET POST等 */
+        corsConfiguration.addAllowedMethod("*");
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
     }
 }

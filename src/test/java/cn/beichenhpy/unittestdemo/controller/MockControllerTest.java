4f7ff5946c6c9ccb90c5e74cc01544ceaab947db
beichenhpy <beichenhpy@outlook.com>
2021-02-03 16:17:04 +0800

    添加rollback()

--- src/test/java/cn/beichenhpy/unittestdemo/controller/MockControllerTest.java
+++ src/test/java/cn/beichenhpy/unittestdemo/controller/MockControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -22,6 +23,7 @@ import java.nio.charset.StandardCharsets;
 @Slf4j
 @SpringBootTest
 @AutoConfigureMockMvc
+@Rollback()
 @RunWith(SpringRunner.class)
 class MockControllerTest {
 
